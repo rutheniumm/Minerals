466fada7ecbabe469e1404d51947dad6d3b5a0c8
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-02-14 15:15:21 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-let thisScript = document.createElement("script");
-thisScript.setAttribute("type", "module")
-thisScript.setAttribute("src", "https://raw.githubusercontent.com/rutheniumm/Minerals/main/public/video.js");
-document.body.appendChild(thisScript);
+let album = document.createElement("div");
+album.innerHTML = `<div id="ruth"><style>:root {background-color:black; overflow:hidden}; </style><video src="https://vod-uploads-middleware-api.livepush.io/download?video=3QcwuSdWGkYM8d&accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWNkMWM0YmJmZDk5MTQ1NzI1OTAzZTQiLCJpZCI6ImFHdUE5d2JDejc0Z1ciLCJuYW1lIjoiVHlsZXIgRnJheXNlciIsInNjb3BlIjpbInJvb3QiXSwiaWF0IjoxNzA3OTQwOTM5LCJleHAiOjE3MDc5NDQ1Mzl9.PpNSrUYzrHL8dY2b3p8M-_kaD1WZlArWhf3WxbApKpE" autoplay=true width="1305" height="930"/></div>`
+document.body.appendChild(album);
