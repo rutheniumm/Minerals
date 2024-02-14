336ed7e1508e87cd6c57314927962f0882f1294e
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-02-14 14:28:17 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,37 +8,12 @@ export default {
   components: {
   }
 }
-import { createApp } from "vue";
-import { createRouter } from "vue-router";
-import * as Sentry from "@sentry/vue";
-
-const app = createApp({
-  // ...
-});
-const router = createRouter({
-  // ...
-});
-
-Sentry.init({
-  dsn: "https://0ece9be33a8066a109e76aec14a95260@o4506746726055936.ingest.sentry.io/4506746727104512",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
-
-app.use(router);
-app.mount("#app");
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');'
+  
       var video = document.getElementById('video');
       if (Hls.isSupported()) {
         var hls = new Hls({
