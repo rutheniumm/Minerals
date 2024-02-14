d55b8d45f063cdff7d2d0f48f916340812b5b8f0
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-02-14 14:46:58 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 let thisScript = document.createElement("script");
 thisScript.setAttribute("type", "module")
-thisScript.setAttribute("src", "https://raw.githubusercontent.com/rutheniumm/Minerals/main/public/video.js?token=GHSAT0AAAAAACOB3ZAT447OLAPEGEVFM24MZONC6TQ");
+thisScript.setAttribute("src", "https://raw.githubusercontent.com/rutheniumm/Minerals/main/public/video.js");
 document.body.appendChild(thisScript);
